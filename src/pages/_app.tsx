63c376bb5f7e962fb1dc787a3ace5c94592f589b
Gilney N. Mathias <gilney_salvo@hotmail.com>
2022-12-04 16:02:16 -0300

    Add a 'shared' folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,9 @@ import { type Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import { type AppType } from 'next/app';
 
-import { trpc } from '~/utils/trpc';
+import { trpc } from '~/shared/utils/trpc';
 
-import '~/styles/globals.css';
+import '~/shared/styles/globals.css';
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
