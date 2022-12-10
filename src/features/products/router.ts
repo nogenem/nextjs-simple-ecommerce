e870dfbfa4b02ba8b4a0ca39fe17a4395e0dedc7
Gilney N. Mathias <gilney_salvo@hotmail.com>
2022-12-10 13:25:12 -0300

    Refine the "products.home" route' query a bit

--- src/features/products/router.ts
+++ src/features/products/router.ts
@@ -1,17 +1,39 @@
 import { publicProcedure, router } from '~/server/trpc/trpc';
 
 export const productsRouter = router({
-  home: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.product.findMany({
+  home: publicProcedure.query(async ({ ctx }) => {
+    const products = await ctx.prisma.product.findMany({
       include: {
         variants: {
           include: {
             images: true,
+            attributes: true,
+          },
+          where: {
+            available_for_sale: true,
           },
         },
         category: true,
         discount: true,
       },
+      where: {
+        variants: {
+          some: {
+            available_for_sale: true,
+          },
+        },
+      },
+      orderBy: {
+        updated_at: 'desc',
+      },
+    });
+
+    const now = new Date();
+    return products.map((product) => {
+      if (product.discount && product.discount.valid_until < now) {
+        product.discount = null;
+      }
+      return product;
     });
   }),
 });
