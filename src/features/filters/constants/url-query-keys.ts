983a21bd0641926106049dc01c7c0fa693365fd8
Gilney N. Mathias <gilney_salvo@hotmail.com>
2022-12-13 18:16:46 -0300

    Fix validations/scheme

--- src/features/filters/constants/url-query-keys.ts
+++ src/features/filters/constants/url-query-keys.ts
@@ -9,7 +9,7 @@ export const URL_QUERY_KEYS = {
 };
 
 export const URL_QUERY_KEYS_VALIDATIONS = {
-  [URL_QUERY_KEYS.CATEGORY_ID]: z.string(),
+  [URL_QUERY_KEYS.CATEGORY_ID]: z.string().min(1),
   [URL_QUERY_KEYS.MIN_PRICE]: z.union([
     z.coerce.number().gte(0),
     z.string().length(0),
@@ -18,8 +18,10 @@ export const URL_QUERY_KEYS_VALIDATIONS = {
     z.coerce.number().gte(1),
     z.string().length(0),
   ]),
-  [URL_QUERY_KEYS.COLOR_ID]: z.string(),
-  [URL_QUERY_KEYS.SIZE_ID]: z.string(),
+  [URL_QUERY_KEYS.COLOR_ID]: z.string().min(1),
+  [URL_QUERY_KEYS.SIZE_ID]: z.string().min(1),
 };
 
-export const URL_QUERY_KEYS_SCHEME = z.object(URL_QUERY_KEYS_VALIDATIONS);
+export const URL_QUERY_KEYS_SCHEME = z
+  .object(URL_QUERY_KEYS_VALIDATIONS)
+  .partial();
