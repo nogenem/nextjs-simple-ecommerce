cce12587674072aeca9b917626d2630fa75657f3
Gilney N. Mathias <gilney_salvo@hotmail.com>
2023-01-10 14:48:52 -0300

    Add a message after the order is paid

--- src/features/paypal/hooks/use-fulfill-paypal-order.ts
+++ src/features/paypal/hooks/use-fulfill-paypal-order.ts
@@ -1,11 +1,22 @@
+import { useToast } from '@chakra-ui/react';
+
 import { trpc } from '~/shared/utils/trpc';
 
 export const useFulfillPaypalOrder = () => {
+  const toast = useToast();
   const utils = trpc.useContext();
 
   const mutation = trpc.paypal.fulfillOrder.useMutation({
     onSuccess: () => {
       utils.orders.invalidate();
+
+      toast({
+        title: 'Order paid successfully!',
+        description: 'Thanks for your purchase.',
+        status: 'success',
+        isClosable: true,
+        duration: 5000,
+      });
     },
   });
 
